f7aa78ee5ac6427388d49c0460d62308e42ededc
Kreline1993 <n.engemann1993@gmail.com>
2025-09-03 13:43:24 +0200

    fixed issues with editing chickens and returning view

--- Migrations/20250827101311_AddParentRelationships.Designer.cs
+++ Migrations/20250827101311_AddParentRelationships.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YourChickenGuide.Data;
 
@@ -11,9 +12,11 @@ using YourChickenGuide.Data;
 namespace YourChickenGuide.Migrations
 {
     [DbContext(typeof(ApplicationDBContexts))]
-    partial class ApplicationDBContextsModelSnapshot : ModelSnapshot
+    [Migration("20250827101311_AddParentRelationships")]
+    partial class AddParentRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
